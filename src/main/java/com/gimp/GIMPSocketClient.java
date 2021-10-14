972d8fbff62bf560ca9a6eee2df4e24fcb53411d
David Vorona <davidavorona@gmail.com>
2021-10-13 18:05:05 -0700

    Fix socket.io dependency issues, establish basic connection

--- src/main/java/com/gimp/GIMPSocketClient.java
+++ src/main/java/com/gimp/GIMPSocketClient.java
@@ -1,16 +1,19 @@
 package com.gimp;
 
 import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
 import io.socket.engineio.client.transports.Polling;
 import io.socket.engineio.client.transports.WebSocket;
 import java.net.*;
 import io.socket.client.IO;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
 
 @Slf4j
 public class GIMPSocketClient
 {
+	@Getter(AccessLevel.PACKAGE)
 	private Socket socket;
 
 	public void connect(String ip, int port)
@@ -41,14 +44,41 @@ public class GIMPSocketClient
 			.setAuth(null)
 			.build();
 		socket = IO.socket(uri, options);
+		socket.connect();
 
-		socket.on("pong", args -> {
-			log.info(String.valueOf(args));
+		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener()
+		{
+			@Override
+			public void call(Object... args)
+			{
+				log.info(socket.id() + " connected");
+				// TODO: below should work, why doesn't it?
+				socket.emit("ping");
+			}
+		});
+
+		socket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener()
+		{
+			@Override
+			public void call(Object... args)
+			{
+				log.info(socket.id() + " disconnected"); // null
+			}
+		});
+
+		String EVENT_PONG = "pong";
+		socket.on(EVENT_PONG, new Emitter.Listener()
+		{
+			@Override
+			public void call(Object... args)
+			{
+				log.info("pong");
+			}
 		});
 	}
 
-	public void send(String msg)
+	public void send(String msg, String data)
 	{
-		socket.emit(msg);
+		socket.emit(msg, data);
 	}
 }
